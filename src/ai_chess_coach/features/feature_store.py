33af830fb611dd47fbcc002e65dc92b580308468
MarshyKid <heikkoroni2003@gmail.com>
2026-06-13 02:12:48 +0800

    minor fix

--- src/ai_chess_coach/features/feature_store.py
+++ src/ai_chess_coach/features/feature_store.py
@@ -86,7 +86,7 @@ class FeatureStore:
 
             attacker_squares = self._board.attackers(not target_piece.color, target_square)
             attackers: list[AttackerInfo] = []
-            for attacker_square in chess.SQUARES:
+            for attacker_square in attacker_squares:
                 if attacker_square not in attacker_squares:
                     continue
 
